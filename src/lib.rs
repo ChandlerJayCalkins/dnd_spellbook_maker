2a603ef2c5096412a766e6666c1461fc7f61fd55
Chandler Calkins <chandlerjaycalkins@gmail.com>
2023-12-31 16:53:25 -0800

    Added some comments
    Also changed the name of the output pdf document

--- src/lib.rs
+++ src/lib.rs
@@ -90,7 +90,7 @@ pub fn printpdf_test() -> Result<(), Box<dyn std::error::Error>>
     // Get PdfLayerReference from PdfLayerIndex
 	let cover_layer_ref = doc.get_page(cover_page).get_layer(cover_layer);
 
-	// Add image to document layer
+	// Add the background image to the page
 	img1.add_to_layer(cover_layer_ref.clone(), img_transform);
 
     // Define text
@@ -103,22 +103,36 @@ pub fn printpdf_test() -> Result<(), Box<dyn std::error::Error>>
     cover_layer_ref.use_text(format!("{} {}", width, text), title_font_size as f64, Mm(10.0), Mm(200.0), &regular_font);
 
 	// Add next pages
+
+	// Create vec of spells for testing
 	let spell_list = vec![&phb_spells::fire_bolt, &phb_spells::fireball];
+	
+	// Counter variable for naming each layer incrementally
 	let mut layer_count = 1;
+
+	// Loop through each spell
 	for spell in spell_list
 	{
+		// Create a new image since cloning the old one isn't allowed for some reason
 		let img = Image::from_dynamic_image(&img_data.clone());
+		// Create a new page
 		let (page, layer) = doc.add_page(Mm(page_width), Mm(page_height), format!("Layer {}", layer_count));
+		// Create a new bookmark for this page
 		doc.add_bookmark(spell.name, page);
+		// Get a reference to the layer for this page
 		let layer_ref = doc.get_page(page).get_layer(layer);
+		// Add the background image to the page
 		img.add_to_layer(layer_ref.clone(), img_transform);
+		// Calculate the text width of the spell's name
 		let width = calc_text_width(&regular_font_size_data, &header_font_scale, &spell.name);
+		// Add text to the page
 		layer_ref.use_text(format!("{} {}", width, spell.name), header_font_size as f64, Mm(10.0), Mm(280.0), &regular_font);
+		// Increment the layer counter
 		layer_count += 1;
 	}
 
     // Save the document to a file
-    let file = std::fs::File::create("output.pdf")?;
+    let file = std::fs::File::create("Spellbook.pdf")?;
     doc.save(&mut std::io::BufWriter::new(file))?;
 
     Ok(())
