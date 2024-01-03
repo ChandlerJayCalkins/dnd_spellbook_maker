1249ed56c86b4fe1305a0068eb3523e7616e96de
Chandler Calkins <chandlerjaycalkins@gmail.com>
2024-01-02 16:42:17 -0800

    Fixed a typo in a spell description

--- src/phb_spells.rs
+++ src/phb_spells.rs
@@ -30,7 +30,7 @@ pub static fireball: Spell = Spell
 	has_somantic_component: true,
 	material_components: Some("a tiny ball of bat guano and sulfur"),
 	duration: Duration::Instant,
-	description: "A bright streak flashes from your pointing finger to a point you choose within range and then blossoms with a low roar into an explosion of flame. Each creature in a 20-foot-radius sphere centered on that point must make a Dexterity saving throw. A target takes 8d6 fire damage on a failed save, or half as mueh damage on a successful one.
+	description: "A bright streak flashes from your pointing finger to a point you choose within range and then blossoms with a low roar into an explosion of flame. Each creature in a 20-foot-radius sphere centered on that point must make a Dexterity saving throw. A target takes 8d6 fire damage on a failed save, or half as much damage on a successful one.
 	The fire spreads around forners. It ignites flammable objects in the area that aren't being worn or carried.",
 	upcast_description: Some("When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d6 for eaeh slot level above 3rd.")
 };
