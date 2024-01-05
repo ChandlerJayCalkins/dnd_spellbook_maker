a883ae676a23a9fa24ddc334df179b17efeec6d1
Chandler Calkins <chandlerjaycalkins@gmail.com>
2024-01-04 19:00:46 -0800

    Added another spell

--- src/phb_spells.rs
+++ src/phb_spells.rs
@@ -2535,11 +2535,29 @@ pub static FIRE_STORM: Spell = Spell
 	upcast_description: None
 };
 
+pub static FLAME_BLADE: Spell = Spell
+{
+	name: "Flame Blade",
+	level: Level::Level2,
+	school: MagicSchool::Evocation,
+	is_ritual: false,
+	casting_time: CastingTime::BonusAction,
+	range: Range::Yourself(None),
+	has_v_component: true,
+	has_s_component: true,
+	m_components: Some("leaf of sumac"),
+	duration: Duration::Minutes(10, true),
+	description: "You evoke a fiery blade in your free hand. The blade is similar in size and shape to a scimitar, and it lasts for the duration. If you let go of the blade, it disappears, but you can evoke the blade again as a bonus action.
+	You can use your action to make a melee spell attack with the fiery blade. On a hit, the target takes 3d6 fire damage.
+	The flaming blade sheds bright light in a 10-foot radius and dim light for an additional 10 feet.",
+	upcast_description: Some("When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d6 for every two slot levels above 2nd.")
+};
+
 // -----------------------------------------------------------------------------------------------------
 // DONT FORGET TO ADD TO THE SPELL LIST
 // -----------------------------------------------------------------------------------------------------
 
-pub static SPELL_LIST: [&Spell; 138] =
+pub static SPELL_LIST: [&Spell; 139] =
 [
 	&ACID_SPLASH, &AID, &ALARM, &ALTER_SELF, &ANIMAL_FRIENDSHIP, &ANIMAL_MESSENGER,
 	&ANIMAL_SHAPES, &ANIMATE_DEAD, &ANIMATE_OBJECTS, &ANTILIFE_SHELL,
@@ -2571,5 +2589,5 @@ pub static SPELL_LIST: [&Spell; 138] =
 	&EVARDS_BLACK_TENTACLES, &EXPEDITIOUS_RETREAT, &EYEBITE, &FABRICATE,
 	&FAERIE_FIRE, &FALSE_LIFE, &FEAR, &FEATHER_FALL, &FEEBLEMIND, &FEIGN_DEATH,
 	&FIND_FAMILIAR, &FIND_STEED, &FIND_THE_PATH, &FIND_TRAPS, &FINGER_OF_DEATH,
-	&FIRE_SHIELD, &FIRE_STORM
+	&FIRE_SHIELD, &FIRE_STORM, &FLAME_BLADE
 ];
