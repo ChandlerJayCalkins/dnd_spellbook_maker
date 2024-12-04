7fa083d1c29984b3bc297ad0d96b2be43ae47400
Chandler Calkins <chandlerjaycalkins@gmail.com>
2024-12-03 22:40:31 -0800

    Updated the todo

--- src/lib.rs
+++ src/lib.rs
@@ -14,9 +14,6 @@ mod tests;
 pub mod utils;
 
 // TODO
-// 1. Make it so SpellbookWriter can change to a new PDF document / spellbook using the same other settings.
-// This might make the `generate_spellbook` function redundant, but that could be a good thing.
-// 2. Make it so SpellbookWriters immediately create the title page when constructed or upon using a new pdf doc.
-// 3. Finish writing SpellbookWriter constrcutor and then write a test to make sure it works.
-// 4. Rewrite `write_spell_description` function to be combined with `write_textbox` so tokens get parsed and written
+// 1. Make it so SpellbookWriters immediately create the title page when constructed or upon using a new pdf doc.
+// 2. Rewrite `write_spell_description` function to be combined with `write_textbox` so tokens get parsed and written
 // at the same time. Make it so text gets written when it either switches fonts or gets too long to fit on the page.
