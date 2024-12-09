82973f24eac0ac4415a06ecab3808bef72586612
Chandler Calkins <chandlerjaycalkins@gmail.com>
2024-12-09 03:25:29 -0800

    Updated todo

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,6 @@ mod tests;
 pub mod utils;
 
 // TODO
-// 1. Make it so single tokens that are too long get hyphenated using binary search of length of token
-// 2. Add table parsing and writing
-// 3. Add all 2024 Player's Handbook spells
-// 4. Make it so spells can have stat blocks in them
+// 1. Add table parsing and writing
+// 2. Add all 2024 Player's Handbook spells
+// 3. Make it so spells can have stat blocks in them
